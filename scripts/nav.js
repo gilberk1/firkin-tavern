1fff86658945dc9dcb4b4ad7ac83ca5d062bd1b7
Kari Gilbertson <kari.a.gilbertson@gmail.com>
2015-12-05 20:52:07 -0500

    navigation fixes

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,13 +1,31 @@
-(function(){
-  var hamburger = document.getElementById('navIcon');
-  var menu = document.getElementById('menu');
+// (function(){
+//   var hamburger = document.getElementById('navIcon');
+//   var menu = document.getElementById('menu');
 
-  hamburger.addEventListener("click", function(){
-    if(menu.classList.contains('shown')) {
-      menu.classList.remove('shown')
-    } else {
-      menu.classList.add('shown')
-    }
-  });
+//   hamburger.addEventListener("click", function(){
+//     if(menu.classList.contains('shown')) {
+//       menu.classList.remove('shown')
+//     } else {
+//       menu.classList.add('shown')
+//     }
+//   });
 
-})();
+// })();
+
+( function( $ ) {
+$( document ).ready(function() {
+// Cache the elements we'll need
+var menu = $('#cssmenu');
+var menuList = menu.find('ul:first');
+var listItems = menu.find('li').not('#responsive-tab');
+
+// Create responsive trigger
+menuList.prepend('<li id="responsive-tab"><a href="#">Menu</a></li>');
+
+// Toggle menu visibility
+menu.on('click', '#responsive-tab', function(){
+	listItems.slideToggle('fast');
+	listItems.addClass('collapsed');
+});
+});
+} )( jQuery );
